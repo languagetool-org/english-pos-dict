3cb6a677e63ad96e92a27015adc6be8ccba5da93
jaumeortola <jaumeortola@gmail.com>
2024-01-05 22:54:47 +0100

    improve build-spelling-dicts.sh

--- build-spelling-dicts.sh
+++ build-spelling-dicts.sh
@@ -25,6 +25,7 @@ mv ./spell-dicts/en_NZ-utf8.txt ./spell-dicts/en_NZ.txt
 
 echo "prepare prohibit.txt"
 cat $src_dict/prohibit.txt | sed 's/#.*//' | sed 's/ \+$//' | sed '/^$/d' > ./spell-dicts/prohibit.txt
+export LC_ALL=C && sort -u -o ./spell-dicts/prohibit.txt ./spell-dicts/prohibit.txt
 
 for variant in AU CA GB NZ US ZA
 do
@@ -33,9 +34,9 @@ do
 	cat $src_dict/spelling.txt | sed 's/#.*//' | sed 's/ \+$//' | sed '/^$/d' >> ./spell-dicts/en_${variant}.txt
 	cat $src_dict/spelling_merged.txt | sed 's/#.*//' | sed 's/ \+$//' | sed '/^$/d' >> ./spell-dicts/en_${variant}.txt
 	cat $src_dict/spelling_en-${variant}.txt | sed 's/#.*//' | sed 's/ \+$//' | sed '/^$/d' >> ./spell-dicts/en_${variant}.txt
-	grep -v -Ff ./spell-dicts/prohibit.txt ./spell-dicts/en_${variant}.txt > ./spell-dicts/en_${variant}-2.txt
-	mv ./spell-dicts/en_${variant}-2.txt ./spell-dicts/en_${variant}.txt
 	export LC_ALL=C && sort -u -o ./spell-dicts/en_${variant}.txt ./spell-dicts/en_${variant}.txt 
+	comm -23 ./spell-dicts/en_${variant}.txt ./spell-dicts/prohibit.txt > ./spell-dicts/en_${variant}-prohibit.txt
+	mv ./spell-dicts/en_${variant}-prohibit.txt ./spell-dicts/en_${variant}.txt
 done
 
 
@@ -46,15 +47,14 @@ target_dir=src/main/resources/org/languagetool/resource/en/hunspell
 for variant in AU CA GB NZ US ZA
 do
 	freqlist=./spell-data/freq/en_wordlist.xml
-	if [ "$my_variable" = "GB" ]; then
-		freqlist=./spell-data/freq/en_gb_wrodlist.xml
+	if [ "$variant" = "GB" ]; then
+		freqlist=./spell-data/freq/en_gb_wordlist.xml
 	fi
-	if [ "$my_variable" = "US" ]; then
-		freqlist=./spell-data/freq/en_us_wrodlist.xml
+	if [ "$variant" = "US" ]; then
+		freqlist=./spell-data/freq/en_us_wordlist.xml
 	fi
-	java -cp $lt_tools org.languagetool.tools.SpellDictionaryBuilder -i ./spell-dicts/en_${variant}.txt -info ${target_dir}/en_${variant}.info -o ${target_dir}/en_${variant}.dict
-
-	#java -cp $jarfile org.languagetool.tools.SpellDictionaryBuilder -i ${targetdict}_spelling.txt -freq $freqlist -info ca-ES_spelling.info -o ${targetdict}_spelling.dict
+	echo "${variant} ${freqlist}"
+	java -cp $lt_tools org.languagetool.tools.SpellDictionaryBuilder -i ./spell-dicts/en_${variant}.txt -freq ${freqlist} -info ${target_dir}/en_${variant}.info -o ${target_dir}/en_${variant}.dict
 done
 
 
